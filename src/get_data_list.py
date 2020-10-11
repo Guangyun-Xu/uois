d81826c5f6191d6a95d988b093c9fb048feefaf4
yumi <785618777@qq.com>
2020-10-12 00:01:49 +0800

    change saved files name, split item no random

--- src/get_data_list.py
+++ src/get_data_list.py
@@ -9,13 +9,13 @@ n_splitTrainList = 1000
 
 date = 1010
 
-dataListPath = os.path.join(trainPath, 'dataList_{}.txt'.format(date))
-trainListPath = os.path.join(trainPath, 'trainList_{}.txt'.format(date))
-trainListSplitPath = os.path.join(trainPath, 'trainListSplit{}_{}.txt'.format(n_splitTrainList, date))
-validListPath = os.path.join(testPath, 'validList_{}.txt'.format(date))
+dataListPath = os.path.join(trainPath, 'data_list_{}.txt'.format(date))
+trainListPath = os.path.join(trainPath, 'train_list_{}.txt'.format(date))
+trainListSplitPath = os.path.join(trainPath, 'train_list_split{}_{}.txt'.format(n_splitTrainList, date))
+validListPath = os.path.join(testPath, 'valid_list_{}.txt'.format(date))
 dataList = open(dataListPath, 'w')
 trainList = open(trainListPath, 'w')
-trainListSplit = open(trainListSplitPath, 'w')
+# trainListSplit = open(trainListSplitPath, 'w')
 validList = open(validListPath, 'w')
 
 files = os.listdir(trainPath)
@@ -37,10 +37,10 @@ dataList.close()
 
 with open(dataListPath, 'r') as f:
     data = f.readlines()
-    np.random.seed(666)
-    np.random.shuffle(data)
+    # np.random.seed(666)
+    # np.random.shuffle(data)
     number = len(data)
-    trainPoint = int(number * 0.8)
+    trainPoint = int(number * 0.9)
     for i, image in enumerate(data):
         if i < trainPoint:
             trainList.write(image)
